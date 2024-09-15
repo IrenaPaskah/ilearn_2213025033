81e68a6c2974c05c668282241039f2606b9997ad
irena <irenamargareth18@gmail.com>
2024-09-16 03:14:58 +0700

    update file dart & inisiasi yaml

--- ilearn_2213025033/lib/login.dart
+++ ilearn_2213025033/lib/login.dart
@@ -86,7 +86,7 @@ class LoginPage extends StatelessWidget {
             ),
             const SizedBox(height: 34),
 
-            // Forgot Password Linkeuuu
+            // Forgot Password Linkeuu
             Center(
               child: GestureDetector(
                 onTap: () {
